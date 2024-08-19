05a21e71cd6a4d507780c71dd4fed778e7e85bd0
HarryFett2k7 <95986296+HarryFett2k7@users.noreply.github.com>
2024-08-19 22:33:40 +0100

    Add files via upload

---
+++ HarrysHub/SimplePage/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PIL import Image
+
+st.header("Harry's Hub", divider="rainbow" )
+st.subheader("All of my current tools/games.")
+
+st.markdown("This is a test while the website is under development")
+
+st.link_button("Percentage Calculator", "https://percentagecalculator.streamlit.app/")
