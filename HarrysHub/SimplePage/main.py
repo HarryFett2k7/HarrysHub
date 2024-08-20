204f36088d36d3deb0c21b949da810a3b5e23985
HarryFett2k7 <95986296+HarryFett2k7@users.noreply.github.com>
2024-08-20 12:06:09 +0100

    Update main.py

--- HarrysHub/SimplePage/main.py
+++ HarrysHub/SimplePage/main.py
@@ -8,3 +8,4 @@ st.markdown("This is a test while the website is under development")
 
 st.link_button("Percentage Calculator", "https://percentagecalculator.streamlit.app/")
 st.link_button("Number Guesser", "https://numberguesser.streamlit.app")
+st.link_button("Binary Convertor", "https://binaryconvertor.streamlit.app")
