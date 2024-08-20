296373ea8616e472dda7d37a67ec6696b6e1a001
HarryFett2k7 <95986296+HarryFett2k7@users.noreply.github.com>
2024-08-20 12:08:46 +0100

    Update main.py

--- HarrysHub/SimplePage/main.py
+++ HarrysHub/SimplePage/main.py
@@ -2,10 +2,13 @@ import streamlit as st
 from PIL import Image
 
 st.header("Harry's Hub", divider="rainbow" )
-st.subheader("All of my current tools/games.")
-
-st.markdown("This is a test while the website is under development")
+st.subheader("All of my current tools.")
 
 st.link_button("Percentage Calculator", "https://percentagecalculator.streamlit.app/")
-st.link_button("Number Guesser", "https://numberguesser.streamlit.app")
 st.link_button("Binary Convertor", "https://binaryconvertor.streamlit.app")
+
+st.subheader("All of my current games.")
+
+st.link_button("Number Guesser", "https://numberguesser.streamlit.app")
+
+st.markdown("This is a test while the website is under development")
